9f301d266ca3a05d2ae56581f22b8f59e612f6e1
杉山 雄紀 <ysugiyama@in.techfirm.co.jp>
2017-12-06 18:47:32 +0900

    Initial commit

---
+++ app/src/main/java/com/ys/raspberrypi/MyFirebaseInstanceIDService.java
@@ -0,0 +1,30 @@
+package com.ys.raspberrypi;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import static android.content.ContentValues.TAG;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    public MyFirebaseInstanceIDService() {
+    }
+
+
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        //sendRegistrationToServer(refreshedToken);
+    }
+
+}
